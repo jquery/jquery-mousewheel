571ec7e0c842cff893b058fc2c1d53a8ecabd777
XhmikosR <xhmikosr@users.sourceforge.net>
2013-02-24 12:27:44 +0200

    jquery.mousewheel.js: keep the style consistent

--- jquery.mousewheel.js
+++ jquery.mousewheel.js
@@ -11,7 +11,7 @@
  */
 
 (function (factory) {
-    if (typeof define === 'function' && define.amd) {
+    if ( typeof define === 'function' && define.amd ) {
         // AMD. Register as an anonymous module.
         define(['jquery'], factory);
     } else {
@@ -24,8 +24,8 @@
     var toBind = 'onwheel' in document || document.documentMode >= 9 ? ['wheel'] : ['mousewheel', 'DomMouseScroll', 'MozMousePixelScroll'];
     var lowestDelta, lowestDeltaXY;
 
-    if ($.event.fixHooks) {
-        for ( var i=toFix.length; i; ) {
+    if ( $.event.fixHooks ) {
+        for ( var i = toFix.length; i; ) {
             $.event.fixHooks[ toFix[--i] ] = $.event.mouseHooks;
         }
     }
@@ -33,7 +33,7 @@
     $.event.special.mousewheel = {
         setup: function() {
             if ( this.addEventListener ) {
-                for ( var i=toBind.length; i; ) {
+                for ( var i = toBind.length; i; ) {
                     this.addEventListener( toBind[--i], handler, false );
                 }
             } else {
@@ -43,7 +43,7 @@
 
         teardown: function() {
             if ( this.removeEventListener ) {
-                for ( var i=toBind.length; i; ) {
+                for ( var i = toBind.length; i; ) {
                     this.removeEventListener( toBind[--i], handler, false );
                 }
             } else {
@@ -64,13 +64,20 @@
 
 
     function handler(event) {
-        var orgEvent = event || window.event, args = [].slice.call( arguments, 1 ), delta = 0, deltaX = 0, deltaY = 0, absDelta = 0, absDeltaXY = 0, fn;
+        var orgEvent = event || window.event,
+            args = [].slice.call(arguments, 1),
+            delta = 0,
+            deltaX = 0,
+            deltaY = 0,
+            absDelta = 0,
+            absDeltaXY = 0,
+            fn;
         event = $.event.fix(orgEvent);
         event.type = "mousewheel";
 
         // Old school scrollwheel delta
-        if ( orgEvent.wheelDelta ) { delta = orgEvent.wheelDelta;  }
-        if ( orgEvent.detail     ) { delta = orgEvent.detail * -1; }
+        if ( orgEvent.wheelDelta ) { delta = orgEvent.wheelDelta; }
+        if ( orgEvent.detail )     { delta = orgEvent.detail * -1; }
 
         // New school wheel delta (wheel event)
         if ( orgEvent.deltaY ) {
@@ -83,20 +90,20 @@
         }
 
         // Webkit
-        if ( orgEvent.wheelDeltaY !== undefined ) { deltaY = orgEvent.wheelDeltaY;      }
+        if ( orgEvent.wheelDeltaY !== undefined ) { deltaY = orgEvent.wheelDeltaY; }
         if ( orgEvent.wheelDeltaX !== undefined ) { deltaX = orgEvent.wheelDeltaX * -1; }
 
         // Look for lowest delta to normalize the delta values
         absDelta = Math.abs(delta);
         if ( !lowestDelta || absDelta < lowestDelta ) { lowestDelta = absDelta; }
-        absDeltaXY = Math.max( Math.abs(deltaY), Math.abs(deltaX) );
+        absDeltaXY = Math.max(Math.abs(deltaY), Math.abs(deltaX));
         if ( !lowestDeltaXY || absDeltaXY < lowestDeltaXY ) { lowestDeltaXY = absDeltaXY; }
 
         // Get a whole value for the deltas
         fn = delta > 0 ? 'floor' : 'ceil';
-        delta  = Math[fn](delta/lowestDelta);
-        deltaX = Math[fn](deltaX/lowestDeltaXY);
-        deltaY = Math[fn](deltaY/lowestDeltaXY);
+        delta  = Math[fn](delta / lowestDelta);
+        deltaX = Math[fn](deltaX / lowestDeltaXY);
+        deltaY = Math[fn](deltaY / lowestDeltaXY);
 
         // Add event and delta to the front of the arguments
         args.unshift(event, delta, deltaX, deltaY);
