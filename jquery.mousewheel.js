215019ee422217ee349bdea5cb98b49b5598a049
Brandon Aaron <hello.brandon@aaron.sh>
2013-10-18 18:46:53 -0500

    update version to 3.1.4-pre

--- jquery.mousewheel.js
+++ jquery.mousewheel.js
@@ -1,11 +1,7 @@
-/*! Copyright (c) 2013 Brandon Aaron (http://brandonaaron.net)
+/*! Copyright (c) 2013 Brandon Aaron (http://brandon.aaron.sh)
  * Licensed under the MIT License (LICENSE.txt).
  *
- * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
- * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
- * Thanks to: Seamus Leahy for adding deltaX and deltaY
- *
- * Version: 3.1.3
+ * Version: 3.1.4-pre
  *
  * Requires: 1.2.2+
  */
