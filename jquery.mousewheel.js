ced8a7b3b8a8b004354d93cd3e727bb5f0cbc75d
Brandon Aaron <hello.brandon@aaron.sh>
2013-11-13 08:50:27 -0500

    Bumping version to 3.1.5-pre

--- jquery.mousewheel.js
+++ jquery.mousewheel.js
@@ -1,7 +1,7 @@
 /*! Copyright (c) 2013 Brandon Aaron (http://brandon.aaron.sh)
  * Licensed under the MIT License (LICENSE.txt).
  *
- * Version: 3.1.4
+ * Version: 3.1.5-pre
  *
  * Requires: 1.2.2+
  */
