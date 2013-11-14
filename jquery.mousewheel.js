b4001b332081e8325bccb7c0315aa7a6fc6e7805
Brandon Aaron <hello.brandon@aaron.sh>
2013-11-14 16:54:11 -0500

    Support debounce and throttle behaviors via the data object. Ex: $(...).on('mousewheel', { mousewheel: { behavior: 'throttle', delay: 100 } }, function(event) {});

--- jquery.mousewheel.js
+++ jquery.mousewheel.js
@@ -47,6 +47,15 @@
             $.data(this, 'mousewheel-page-height', special.getPageHeight(this));
         },
 
+        add: function(handleObj) {
+            if ( 'mousewheel' in (handleObj.data || {}) &&
+                 'behavior' in (handleObj.data.mousewheel) &&
+                 'delay' in (handleObj.data.mousewheel)
+               ) {
+                $.event.special.mousewheel._delayHandler(handleObj);
+            }
+        },
+
         teardown: function() {
             if ( this.removeEventListener ) {
                 for ( var i = toBind.length; i; ) {
@@ -79,14 +88,35 @@
         },
 
         trigger: function(data, event) {
-          if (!event) {
-            event = data;
-            data = null
-          }
+            if (!event) {
+                event = data;
+                data = null
+            }
 
-          handler.call(this, event);
+            handler.call(this, event);
 
-          return false;
+            return false;
+        },
+
+        _delayHandler: function(handlerObj) {
+            var timeout,
+                scope = this,
+                settings = handlerObj.data.mousewheel,
+                oldHandler = handlerObj.handler,
+                newHandler = function() {
+                    var args = arguments;
+                    var delayed = function() {
+                        oldHandler.apply(scope, args);
+                        timeout = null;
+                    };
+                    if ( settings.behavior === "debounce" && timeout ) {
+                        clearTimeout(timeout);
+                    }
+                    if ( settings.behavior === "throttle" && !timeout || settings.behavior === "debounce" ) {
+                        timeout = setTimeout(delayed, settings.delay);
+                    }
+                };
+            handlerObj.handler = newHandler;
         }
     };
 
