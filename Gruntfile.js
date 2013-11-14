ef315943df79200b264cc95faf3b1d03685c41db
Brandon Aaron <hello.brandon@aaron.sh>
2013-11-14 09:22:06 -0500

    Add grunt-contrib-connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,12 +19,22 @@ module.exports = function(grunt) {
                 src: 'jquery.mousewheel.js',
                 dest: 'build/jquery.mousewheel.min.js'
             }
+        },
+        connect: {
+          server: {
+            options: {
+              hostname: '*',
+              directory: '.',
+              keepalive: true
+            }
+          }
         }
     });
 
     // Load the plugin that provides the 'uglify' task.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     // Default task(s).
     grunt.registerTask('default', ['jshint', 'uglify']);
