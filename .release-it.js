fc760b8691412e90492ff958cb806b03373bb237
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2025-03-18 00:19:53 +0100

    Align code style with jQuery - use tab indents

--- .release-it.js
+++ .release-it.js
@@ -1,29 +1,29 @@
 "use strict";
 
 module.exports = {
-    preReleaseBase: 1,
-    hooks: {
-        "before:init": "bash ./build/release/pre-release.sh",
-        "after:version:bump":
+	preReleaseBase: 1,
+	hooks: {
+		"before:init": "bash ./build/release/pre-release.sh",
+		"after:version:bump":
             "sed -i 's/main\\/AUTHORS.txt/${version}\\/AUTHORS.txt/' package.json",
-        "after:bump": "cross-env VERSION=${version} npm run build",
-        "before:git:release": "git add -f dist/",
-        "after:release": "echo 'Run the following to complete the release:' && " +
+		"after:bump": "cross-env VERSION=${version} npm run build",
+		"before:git:release": "git add -f dist/",
+		"after:release": "echo 'Run the following to complete the release:' && " +
             "echo './build/release/post-release.sh $\{version}'"
-    },
-    git: {
-        commitMessage: "Release: ${version}",
-        getLatestTagFromAllRefs: true,
-        pushRepo: "git@github.com:jquery/jquery-mousewheel.git",
-        requireBranch: "main",
-        requireCleanWorkingDir: true
-    },
-    github: {
-        pushRepo: "git@github.com:jquery/jquery-mousewheel.git",
-        release: true,
-        tokenRef: "JQUERY_GITHUB_TOKEN"
-    },
-    npm: {
-        publish: true
-    }
+	},
+	git: {
+		commitMessage: "Release: ${version}",
+		getLatestTagFromAllRefs: true,
+		pushRepo: "git@github.com:jquery/jquery-mousewheel.git",
+		requireBranch: "main",
+		requireCleanWorkingDir: true
+	},
+	github: {
+		pushRepo: "git@github.com:jquery/jquery-mousewheel.git",
+		release: true,
+		tokenRef: "JQUERY_GITHUB_TOKEN"
+	},
+	npm: {
+		publish: true
+	}
 };
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,93 +2,93 @@ import jqueryConfig from "eslint-config-jquery";
 import globals from "globals";
 
 export default [
-    {
-        ignores: [ "dist/**/*.js" ],
-        rules: {
-            ...jqueryConfig.rules,
-            indent: [ "error", 4 ]
-        }
-    },
+	{
+		ignores: [ "dist/**/*.js" ],
+		rules: {
+			...jqueryConfig.rules,
+			indent: [ "error", "tab" ]
+		}
+	},
 
-    {
-        files: [
-            "src/**/*.js",
-            "dist/**/*.js",
-            "test/**/*.js"
-        ],
-        languageOptions: {
+	{
+		files: [
+			"src/**/*.js",
+			"dist/**/*.js",
+			"test/**/*.js"
+		],
+		languageOptions: {
 
-            // Support: IE 9 - 11+
-            ecmaVersion: 5,
+			// Support: IE 9 - 11+
+			ecmaVersion: 5,
 
-            sourceType: "script",
+			sourceType: "script",
 
-            // Support: Non-browser envs like jsdom
-            // Don't include `globals.browser`, take browser globals from `window`.
-            globals: {
-                window: "readonly",
-                define: "readonly",
-                module: "readonly",
-                jQuery: "readonly"
-            }
-        }
-    },
+			// Support: Non-browser envs like jsdom
+			// Don't include `globals.browser`, take browser globals from `window`.
+			globals: {
+				window: "readonly",
+				define: "readonly",
+				module: "readonly",
+				jQuery: "readonly"
+			}
+		}
+	},
 
-    {
-        files: [ "src/**/*.js" ],
-        rules: {
-            strict: [ "error", "function" ],
-            indent: [
-                "error",
-                4,
-                {
+	{
+		files: [ "src/**/*.js" ],
+		rules: {
+			strict: [ "error", "function" ],
+			indent: [
+				"error",
+				"tab",
+				{
 
-                    // This makes it so code within the wrapper is not indented.
-                    ignoredNodes: [
-                        "Program > FunctionDeclaration > *"
-                    ]
-                }
-            ]
-        }
-    },
+					// This makes it so code within the wrapper is not indented.
+					ignoredNodes: [
+						"Program > FunctionDeclaration > *"
+					]
+				}
+			]
+		}
+	},
 
-    {
-        files: [ "test/**/*.js" ],
-        languageOptions: {
-            globals: {
-                QUnit: "readonly"
-            }
-        },
-        rules: {
-            strict: [ "error", "global" ]
-        }
-    },
+	{
+		files: [ "test/**/*.js" ],
+		languageOptions: {
+			globals: {
+				QUnit: "readonly"
+			}
+		},
+		rules: {
+			strict: [ "error", "global" ]
+		}
+	},
 
-    {
-        files: [ "*.js" ],
-        languageOptions: {
-            ecmaVersion: "latest",
-            sourceType: "commonjs",
-            globals: {
-                ...globals.node
-            }
-        },
-        rules: {
-            ...jqueryConfig.rules
-        }
-    },
+	{
+		files: [ "*.js" ],
+		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "commonjs",
+			globals: {
+				...globals.node
+			}
+		},
+		rules: {
+			...jqueryConfig.rules
+		}
+	},
 
-    {
-        files: [ "*.mjs", "build/**/*.mjs" ],
-        languageOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
-            globals: {
-                ...globals.node
-            }
-        },
-        rules: {
-            ...jqueryConfig.rules
-        }
-    }
+	{
+		files: [ "*.mjs", "build/**/*.mjs" ],
+		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "module",
+			globals: {
+				...globals.node
+			}
+		},
+		rules: {
+			...jqueryConfig.rules
+		}
+	}
 ];
