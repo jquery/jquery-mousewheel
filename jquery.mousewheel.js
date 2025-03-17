44ebf66ae3ddf73262f6699e70c29ac346dafe7f
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2025-03-17 22:25:27 +0100

    Version 3.2.0

--- jquery.mousewheel.js
+++ jquery.mousewheel.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Mousewheel 3.1.13
+ * jQuery Mousewheel 3.2.0
  * Copyright OpenJS Foundation and other contributors
  */
 
@@ -36,7 +36,7 @@
     }
 
     var special = $.event.special.mousewheel = {
-        version: "3.1.12",
+        version: "3.2.0",
 
         setup: function() {
             if ( this.addEventListener ) {
