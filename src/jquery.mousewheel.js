82e44a76897142391fcbad878e41948128705c26
summercms <ayumih880@gmail.com>
2025-08-10 23:23:43 +0100

    Added back tab indentation

--- src/jquery.mousewheel.js
+++ src/jquery.mousewheel.js
@@ -6,184 +6,157 @@
  * Released under the MIT license
  * https://jquery.org/license
  */
+
 ( function( factory ) {
-    "use strict";
+	"use strict";
 
-    if ( typeof define === "function" && define.amd ) {
+	if ( typeof define === "function" && define.amd ) {
 
-        // AMD. Register as an anonymous module.
-        define( [ "jquery" ], factory );
-    } else if ( typeof exports === "object" ) {
+		// AMD. Register as an anonymous module.
+		define( [ "jquery" ], factory );
+	} else if ( typeof exports === "object" ) {
 
-        // Node/CommonJS style for Browserify
-        module.exports = factory;
-    } else {
+		// Node/CommonJS style for Browserify
+		module.exports = factory;
+	} else {
 
-        // Browser globals
-        factory( jQuery );
-    }
+		// Browser globals
+		factory( jQuery );
+	}
 } )( function( $ ) {
-    "use strict";
+	"use strict";
 
-    var nullLowestDeltaTimeout, lowestDelta,
-        slice  = Array.prototype.slice;
+	var nullLowestDeltaTimeout, lowestDelta,
+		slice  = Array.prototype.slice;
 
-    if ( $.event.fixHooks ) {
-        $.event.fixHooks.wheel = $.event.mouseHooks;
-    }
+	if ( $.event.fixHooks ) {
+		$.event.fixHooks.wheel = $.event.mouseHooks;
+	}
 
-    var special = $.event.special.mousewheel = {
-        version: "@VERSION",
+	var special = $.event.special.mousewheel = {
+		version: "@VERSION",
 
-        setup: function() {
-            // Use passive event listeners if supported
-            var passiveSupported = false;
-            try {
-                var opts = Object.defineProperty({}, "passive", {
-                    get: function() {
-                        passiveSupported = true;
-                    }
-                });
-                window.addEventListener("test", null, opts);
-            } catch (e) {}
-            this.addEventListener(
-                "wheel",
-                handler,
-                passiveSupported ? { passive: true } : false
-            );
+		setup: function() {
+			this.addEventListener( "wheel", handler, false );
 
-            // Store the line height and page height for this particular element
-            $.data( this, "mousewheel-line-height", special.getLineHeight( this ) );
-            $.data( this, "mousewheel-page-height", special.getPageHeight( this ) );
-        },
+			// Store the line height and page height for this particular element
+			$.data( this, "mousewheel-line-height", special.getLineHeight( this ) );
+			$.data( this, "mousewheel-page-height", special.getPageHeight( this ) );
+		},
 
-        teardown: function() {
-            // Use passive event listeners if supported
-            var passiveSupported = false;
-            try {
-                var opts = Object.defineProperty({}, "passive", {
-                    get: function() {
-                        passiveSupported = true;
-                    }
-                });
-                window.addEventListener("test", null, opts);
-            } catch (e) {}
-            this.removeEventListener(
-                "wheel",
-                handler,
-                passiveSupported ? { passive: true } : false
-            );
+		teardown: function() {
+			this.removeEventListener( "wheel", handler, false );
 
-            // Clean up the data we added to the element
-            $.removeData( this, "mousewheel-line-height" );
-            $.removeData( this, "mousewheel-page-height" );
-        },
+			// Clean up the data we added to the element
+			$.removeData( this, "mousewheel-line-height" );
+			$.removeData( this, "mousewheel-page-height" );
+		},
 
-        getLineHeight: function( elem ) {
-            var $elem = $( elem ),
-                $parent = $elem.offsetParent();
-            if ( !$parent.length ) {
-                $parent = $( "body" );
-            }
-            return parseInt( $parent.css( "fontSize" ), 10 ) ||
-                parseInt( $elem.css( "fontSize" ), 10 ) || 16;
-        },
+		getLineHeight: function( elem ) {
+			var $elem = $( elem ),
+				$parent = $elem.offsetParent();
+			if ( !$parent.length ) {
+				$parent = $( "body" );
+			}
+			return parseInt( $parent.css( "fontSize" ), 10 ) ||
+				parseInt( $elem.css( "fontSize" ), 10 ) || 16;
+		},
 
-        getPageHeight: function( elem ) {
-            return $( elem ).height();
-        },
+		getPageHeight: function( elem ) {
+			return $( elem ).height();
+		},
 
-        settings: {
-            normalizeOffset: true  // calls getBoundingClientRect for each event
-        }
-    };
+		settings: {
+			normalizeOffset: true  // calls getBoundingClientRect for each event
+		}
+	};
 
-    function handler( origEvent ) {
-        var args = slice.call( arguments, 1 ),
-            delta = 0,
-            deltaX = 0,
-            deltaY = 0,
-            absDelta = 0,
-            event = $.event.fix( origEvent );
+	function handler( origEvent ) {
+		var args = slice.call( arguments, 1 ),
+			delta = 0,
+			deltaX = 0,
+			deltaY = 0,
+			absDelta = 0,
+			event = $.event.fix( origEvent );
 
-        event.type = "mousewheel";
+		event.type = "mousewheel";
 
-        // New school wheel delta (wheel event)
-        if ( "deltaY" in origEvent ) {
-            deltaY = origEvent.deltaY * -1;
-            delta  = deltaY;
-        }
-        if ( "deltaX" in origEvent ) {
-            deltaX = origEvent.deltaX;
-            if ( deltaY === 0 ) {
-                delta  = deltaX * -1;
-            }
-        }
+		// New school wheel delta (wheel event)
+		if ( "deltaY" in origEvent ) {
+			deltaY = origEvent.deltaY * -1;
+			delta  = deltaY;
+		}
+		if ( "deltaX" in origEvent ) {
+			deltaX = origEvent.deltaX;
+			if ( deltaY === 0 ) {
+				delta  = deltaX * -1;
+		}
+		}
 
-        // No change actually happened, no reason to go any further
-        if ( deltaY === 0 && deltaX === 0 ) {
-            return;
-        }
+		// No change actually happened, no reason to go any further
+		if ( deltaY === 0 && deltaX === 0 ) {
+			return;
+		}
 
-        // Need to convert lines and pages to pixels if we aren't already in pixels
-        // There are three delta modes:
-        //   * deltaMode 0 is by pixels, nothing to do
-        //   * deltaMode 1 is by lines
-        //   * deltaMode 2 is by pages
-        if ( origEvent.deltaMode === 1 ) {
-            var lineHeight = $.data( this, "mousewheel-line-height" );
-            delta  *= lineHeight;
-            deltaY *= lineHeight;
-            deltaX *= lineHeight;
-        } else if ( origEvent.deltaMode === 2 ) {
-            var pageHeight = $.data( this, "mousewheel-page-height" );
-            delta  *= pageHeight;
-            deltaY *= pageHeight;
-            deltaX *= pageHeight;
-        }
+		// Need to convert lines and pages to pixels if we aren't already in pixels
+		// There are three delta modes:
+		//   * deltaMode 0 is by pixels, nothing to do
+		//   * deltaMode 1 is by lines
+		//   * deltaMode 2 is by pages
+		if ( origEvent.deltaMode === 1 ) {
+			var lineHeight = $.data( this, "mousewheel-line-height" );
+			delta  *= lineHeight;
+			deltaY *= lineHeight;
+			deltaX *= lineHeight;
+		} else if ( origEvent.deltaMode === 2 ) {
+			var pageHeight = $.data( this, "mousewheel-page-height" );
+			delta  *= pageHeight;
+			deltaY *= pageHeight;
+			deltaX *= pageHeight;
+		}
 
-        // Store lowest absolute delta to normalize the delta values
-        absDelta = Math.max( Math.abs( deltaY ), Math.abs( deltaX ) );
+		// Store lowest absolute delta to normalize the delta values
+		absDelta = Math.max( Math.abs( deltaY ), Math.abs( deltaX ) );
 
-        if ( !lowestDelta || absDelta < lowestDelta ) {
-            lowestDelta = absDelta;
-        }
+		if ( !lowestDelta || absDelta < lowestDelta ) {
+			lowestDelta = absDelta;
+		}
 
-        // Get a whole, normalized value for the deltas
-        delta  = Math[ delta  >= 1 ? "floor" : "ceil" ]( delta  / lowestDelta );
-        deltaX = Math[ deltaX >= 1 ? "floor" : "ceil" ]( deltaX / lowestDelta );
-        deltaY = Math[ deltaY >= 1 ? "floor" : "ceil" ]( deltaY / lowestDelta );
+		// Get a whole, normalized value for the deltas
+		delta  = Math[ delta  >= 1 ? "floor" : "ceil" ]( delta  / lowestDelta );
+		deltaX = Math[ deltaX >= 1 ? "floor" : "ceil" ]( deltaX / lowestDelta );
+		deltaY = Math[ deltaY >= 1 ? "floor" : "ceil" ]( deltaY / lowestDelta );
 
-        // Normalise offsetX and offsetY properties
-        if ( special.settings.normalizeOffset && this.getBoundingClientRect ) {
-            var boundingRect = this.getBoundingClientRect();
-            event.offsetX = event.clientX - boundingRect.left;
-            event.offsetY = event.clientY - boundingRect.top;
-        }
+		// Normalise offsetX and offsetY properties
+		if ( special.settings.normalizeOffset && this.getBoundingClientRect ) {
+			var boundingRect = this.getBoundingClientRect();
+			event.offsetX = event.clientX - boundingRect.left;
+			event.offsetY = event.clientY - boundingRect.top;
+		}
 
-        // Add information to the event object
-        event.deltaX = deltaX;
-        event.deltaY = deltaY;
-        event.deltaFactor = lowestDelta;
+		// Add information to the event object
+		event.deltaX = deltaX;
+		event.deltaY = deltaY;
+		event.deltaFactor = lowestDelta;
 
-        // Go ahead and set deltaMode to 0 since we converted to pixels
-        // Although this is a little odd since we overwrite the deltaX/Y
-        // properties with normalized deltas.
-        event.deltaMode = 0;
+		// Go ahead and set deltaMode to 0 since we converted to pixels
+		// Although this is a little odd since we overwrite the deltaX/Y
+		// properties with normalized deltas.
+		event.deltaMode = 0;
 
-        // Add event and delta to the front of the arguments
-        args.unshift( event, delta, deltaX, deltaY );
+		// Add event and delta to the front of the arguments
+		args.unshift( event, delta, deltaX, deltaY );
 
-        // Clear out lowestDelta after sometime to better
-        // handle multiple device types that give different
-        // a different lowestDelta
-        // Ex: trackpad = 3 and mouse wheel = 120
-        window.clearTimeout( nullLowestDeltaTimeout );
-        nullLowestDeltaTimeout = window.setTimeout( function() {
-            lowestDelta = null;
-        }, 200 );
+		// Clear out lowestDelta after sometime to better
+		// handle multiple device types that give different
+		// a different lowestDelta
+		// Ex: trackpad = 3 and mouse wheel = 120
+		window.clearTimeout( nullLowestDeltaTimeout );
+		nullLowestDeltaTimeout = window.setTimeout( function() {
+			lowestDelta = null;
+		}, 200 );
 
-        return $.event.dispatch.apply( this, args );
-    }
+		return $.event.dispatch.apply( this, args );
+	}
 
 } );
